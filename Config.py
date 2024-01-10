ebeba5a1da3a9f25b19be706ece12d659fbf9f16
josedav2912 <82848798+josedav2912@users.noreply.github.com>
2024-01-10 16:36:46 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 0))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", None)  # Change None to your API_HASH
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  # Change None to your BOT_TOKEN
+    # API_ID = int(os.environ.get("API_ID", 0))  # Change 12345 to your API_ID
+    # API_HASH = os.environ.get("API_HASH", None)  # Change None to your API_HASH
+    # BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  # Change None to your BOT_TOKEN
     OWNER_ID = int(os.environ.get("OWNER_ID", 0))  # Change 0 to your OWNER_ID
     OWNER_NAME = os.environ.get("OWNER_NAME", None)  # Change None to your OWNER_NAME
 
